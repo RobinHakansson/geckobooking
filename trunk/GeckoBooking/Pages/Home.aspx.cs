be2337015eb4d767fb2cb43e1dedae24e42bbd6b
eriqa@hotmail.com <eriqa@hotmail.com@6a025af5-b286-4f5d-d798-22a76e72feef>
2014-08-14 07:49:48 +0000

    Kompileringsfel på Default och Home fixat.

--- trunk/GeckoBooking/Pages/Home.aspx.cs
+++ trunk/GeckoBooking/Pages/Home.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace GeckoBooking
 {
-    public partial class Pages_Home : System.Web.UI.Page
+    public partial class Home : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
