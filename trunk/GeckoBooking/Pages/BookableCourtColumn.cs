e3e1399388c51e7a06de788ba1c36e835b051fe4
robin.lars.hakansson@gmail.com <robin.lars.hakansson@gmail.com@6a025af5-b286-4f5d-d798-22a76e72feef>
2014-08-14 17:36:58 +0000

    Här har du

---
+++ trunk/GeckoBooking/Pages/BookableCourtColumn.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GeckoBooking
+{
+    public class BookableCourtColumn : ITemplate
+    {
+        public void InstantiateIn(Control container)
+        {
+            var cBox = new CheckBox();
+            cBox.ID = "SessionCheckBox";
+            container.Controls.Add(cBox);
+        }
+
+    }
+}
