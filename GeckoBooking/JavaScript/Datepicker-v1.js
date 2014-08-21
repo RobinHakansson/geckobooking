d94de983a2a847a7dbc9081d9f0600747c6cf19d
robin.lars.hakansson <robin.lars.hakansson@gmail.com>
2014-08-21 15:19:18 +0000

    CheckboxTesting

--- GeckoBooking/JavaScript/Datepicker-v1.js
+++ GeckoBooking/JavaScript/Datepicker-v1.js
@@ -4,6 +4,10 @@
         //buttonImage: "images/calendar.gif",
         //buttonImageOnly: true,
         //showAnim: "fadeIn",
+        onSelect: function() {
+            $("form").submit();
+            //$(".date").val();
+        },
         firstDay: 1,
         showWeek: true,
         altField: ".date",
