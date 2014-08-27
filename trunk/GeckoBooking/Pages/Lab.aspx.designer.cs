b70923160f7393144f2955d3277248a4615d08a9
eriqa@hotmail.com <eriqa@hotmail.com@6a025af5-b286-4f5d-d798-22a76e72feef>
2014-08-27 20:58:41 +0000

    Confirm-box

--- trunk/GeckoBooking/Pages/Lab.aspx.designer.cs
+++ trunk/GeckoBooking/Pages/Lab.aspx.designer.cs
@@ -92,5 +92,23 @@ namespace GeckoBooking {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.UpdateProgress UpdateProgress1;
+        
+        /// <summary>
+        /// UpdatePanel3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel3;
+        
+        /// <summary>
+        /// Label6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
     }
 }
